9314d824f5691c0f9d2df798ac3d665c433ecbbb
Avdo Sator <avdo.sator@hotmail.com>
2023-12-22 13:14:56 +0100

    Make Campground model and connect app to yelp-camp database

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const methodOverride = require("method-override");
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error!"));
+db.once("open", () => console.log("Connected to database!"));
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded( { extended: "true"}));
-
 
 app.listen(3000, () => console.log("Listening on PORT 3000"));
