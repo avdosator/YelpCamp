34cc7ad18f6eb5e7677d59592e1580463c7606ee
Avdo Sator <avdo.sator@hotmail.com>
2023-12-22 20:21:48 +0100

    Make route for showing particular campground's informations

--- app.js
+++ app.js
@@ -20,4 +20,9 @@ app.get("/campgrounds", async (req, res) => {
     res.render("campgrounds/index", {campgrounds});
 });
 
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", {campground});
+})
+
 app.listen(3000, () => console.log("Listening on PORT 3000"));
