f8e57d391b74d058eddf9fd61df5384e6b7badf0
Avdo Sator <avdo.sator@hotmail.com>
2023-12-27 23:38:10 +0100

    Use ejs-mate package and reorganize code in .ejs templates

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const methodOverride = require("method-override");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
@@ -12,6 +13,7 @@ db.once("open", () => console.log("Connected to database!"));
 const app = express();
 
 app.set("view engine", "ejs");
+app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded( { extended: true}));
 app.use(methodOverride("_method"));
