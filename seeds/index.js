fb8ce3ea26b7249c5aaf27e8bbc23da12916a30b
Avdo Sator <avdo.sator@hotmail.com>
2023-12-28 17:14:08 +0100

    Make changes to campground model and implement that that changes in seeds/index.js file

--- seeds/index.js
+++ seeds/index.js
@@ -14,9 +14,13 @@ const seedDatabase = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const rand1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor((Math.random() * 20) + 10);
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[rand1000].city}, ${cities[rand1000].state}`
+            location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. In tempore recusandae amet id ipsum suscipit iusto consequuntur nam, repellat veritatis pariatur eaque quam similique ab commodi nostrum ut autem repudiandae.",
+            image: "https://images.unsplash.com/photo-1563299796-17596ed6b017?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            price
         });
         await camp.save();
     }
