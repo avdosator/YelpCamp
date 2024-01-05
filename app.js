911c895af4ba4ea6c3e680660ce5c69d4ade3715
Avdo Sator <avdo.sator@hotmail.com>
2024-01-05 13:16:24 +0100

    Add basic error handling route and use it in routes for add new and edit campground

--- app.js
+++ app.js
@@ -32,16 +32,24 @@ app.get("/campgrounds/:id/edit", async(req, res) => {
     res.render("campgrounds/edit", {campground});
 });
 
-app.put("/campgrounds/:id", async (req, res) => {
-    const {id} = req.params;
-    await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new: true});
-    res.redirect(`/campgrounds/${req.params.id}`);
+app.put("/campgrounds/:id", async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new: true});
+        res.redirect(`/campgrounds/${req.params.id}`);
+    } catch(e) {
+        next(e);
+    }
 });
 
-app.post("/campgrounds", async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground.id}`);
+app.post("/campgrounds", async (req, res, next) => {
+    try {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground.id}`);
+    } catch(e) {
+        next(e);
+    }
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
@@ -53,6 +61,10 @@ app.delete("/campgrounds/:id", async (req, res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+});
+
+app.use((err, req, res, next) => {
+    res.send("Something went wrong!");
 });
 
 app.listen(3000, () => console.log("Listening on PORT 3000"));
