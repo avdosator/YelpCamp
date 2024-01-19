c49aef454450332816b69620cf86cf52dcb693d2
Avdo Sator <avdo.sator@hotmail.com>
2024-01-19 20:37:28 +0100

    Implement route for adding a review

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const methodOverride = require("method-override");
 const path = require("path");
 const mongoose = require("mongoose");
-const ExpressError = require("./utils/ExpressError");
 const catchAsync = require("./utils/catchAsync");
 const {campgroundSchema} = require("./schemas");
 const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
 const db = mongoose.connection;
@@ -66,6 +67,15 @@ app.delete("/campgrounds/:id", catchAsync(async (req, res, next) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+}));
+
+app.post("/campgrounds/:id/reviews", catchAsync(async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.unshift(review);
+    await campground.save();
+    await review.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 app.all("*", (req, res, next) => {
