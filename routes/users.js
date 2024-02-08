a691e5d52b044ef6344c8634fbad5666a5fe1017
Avdo Sator <avdo.sator@hotmail.com>
2024-02-08 17:41:41 +0100

    Add route for creating User instance

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get("/register", (req, res) => {
     res.render("users/register");
 });
 
+router.post("/register", async(req, res) => {
+    const { username, email, password} = req.body;
+    const user = new User({ username, email });
+    const registeredUser = await User.register(user, password);
+    req.flash("success", "Welcome to Yelp Camp!");
+    res.redirect("/campgrounds");
+});
+
 module.exports = router;
