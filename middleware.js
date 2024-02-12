564b1db902940f9dd1ecb4934b5673146b10fd24
Avdo Sator <avdo.sator@hotmail.com>
2024-02-12 20:33:47 +0100

    Prevent non-authors to delete reviews

--- middleware.js
+++ middleware.js
@@ -1,9 +1,10 @@
 const { campgroundSchema, reviewSchema } = require("./schemas");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 const ExpressError = require("./utils/ExpressError");
 
-const isLoggedIn =  (req, res, next) => {
-    if(!req.isAuthenticated()) {
+const isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
         req.flash("error", "You must be logged in!");
         req.session.returnTo = req.originalUrl; // we store url in session so we can use it later when we log in
         return res.redirect("/login");
@@ -13,7 +14,7 @@ const isLoggedIn =  (req, res, next) => {
 
 // we store returnTo (originalUrl) in locals because when we login passport.authenticate() will delete session
 const storeReturnTo = (req, res, next) => {
-    if(req.session.returnTo) {
+    if (req.session.returnTo) {
         res.locals.returnTo = req.session.returnTo;
     }
     next();
@@ -39,9 +40,19 @@ const isAuthor = async (req, res, next) => {
     next();
 }
 
+const isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash("error", "You don't have permission to do that!");
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
 const validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
-    if(error) {
+    if (error) {
         const msg = error.details.map(el => el.message).join(",");
         throw new ExpressError(msg, 400);
     } else {
@@ -49,4 +60,11 @@ const validateReview = (req, res, next) => {
     }
 }
 
-module.exports = {isLoggedIn, storeReturnTo, validateCampground, isAuthor, validateReview};
+module.exports = {
+    isLoggedIn,
+    storeReturnTo,
+    validateCampground,
+    isAuthor,
+    isReviewAuthor,
+    validateReview
+};
