35505c4cca99f135c7993272a0bab47cdf2c4183
Avdo Sator <avdo.sator@hotmail.com>
2024-02-12 11:32:19 +0100

    Add basic check if user is author of campground to protect campground editing

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -59,6 +59,11 @@ router.get("/:id/edit", isLoggedIn, catchAsync(async(req, res, next) => {
 
 router.put("/:id", isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const {id} = req.params;
+    const campground = await Campground.findById(id);
+    if(!campground.author.equals(req.user._id)) {
+        req.flash("error", "You don't have permission to do that!");
+        return res.redirect(`/campgrounds/${id}`);
+    }
     await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new: true});
     req.flash("success", "Successfully updated campground!");
     res.redirect(`/campgrounds/${req.params.id}`);
