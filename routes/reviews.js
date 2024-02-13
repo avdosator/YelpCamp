27d1d45d7c1da898362d1c9b05f54a565b19dc92
Avdo Sator <avdo.sator@hotmail.com>
2024-02-13 18:42:59 +0100

    Move all users routes to users controller to make files more readable

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,9 +5,6 @@ const { isLoggedIn, validateReview, isReviewAuthor } = require("../middleware");
 
 const reviews = require("../controllers/reviews");
 
-const Review = require("../models/review");
-const Campground = require("../models/campground");
-
 router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 router.delete("/:reviewId", isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
