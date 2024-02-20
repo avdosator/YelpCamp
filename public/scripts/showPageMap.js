5bf85afbe85f613a7467f578d4556be35bb0f421
Avdo Sator <avdo.sator@hotmail.com>
2024-02-20 16:13:55 +0100

    Remove unnecessary comments

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -3,6 +3,6 @@ mapboxgl.accessToken = mapToken;
         const map = new mapboxgl.Map({
             container: 'map', // container ID
             style: 'mapbox://styles/mapbox/streets-v12', // style URL
-            center: [-74.5, 40], // starting position [lng, lat]
-            zoom: 6, // starting zoom
+            center: [-74.5, 40],
+            zoom: 6,
         });
