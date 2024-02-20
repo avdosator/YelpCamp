daa26860b7e94c00b92b470450057d1faaf2a5e0
Avdo Sator <avdo.sator@hotmail.com>
2024-02-20 23:37:19 +0100

    Solve problems with correct shape of JS objects and JSON objects between browser and server

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -1,12 +1,14 @@
+
 // we need to save process.env.MAPBOX_TOKEN in a variable on show page, and put that in the script, so we can use it here
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40],
-    zoom: 6,
+    // somewhere between server and browser were problems, so we had to call replaceAll like this to correct the images.path
+    center: JSON.parse(campground.replaceAll("\\", "\\\\")).geometry.coordinates,
+    zoom: 10,
 });
 
-new mapboxgl.Marker()
-    .setLngLat([-74.5, 40])
-    .addTo(map)
+ new mapboxgl.Marker()
+     .setLngLat(JSON.parse(campground.replaceAll("\\", "\\\\")).geometry.coordinates)
+     .addTo(map)
