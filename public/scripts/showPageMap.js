133f29292bf514ab8169325cda05ec7334c3f125
Avdo Sator <avdo.sator@hotmail.com>
2024-02-20 16:35:37 +0100

    Add a default marker to the map

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -1,8 +1,12 @@
 // we need to save process.env.MAPBOX_TOKEN in a variable on show page, and put that in the script, so we can use it here
 mapboxgl.accessToken = mapToken;
-        const map = new mapboxgl.Map({
-            container: 'map', // container ID
-            style: 'mapbox://styles/mapbox/streets-v12', // style URL
-            center: [-74.5, 40],
-            zoom: 6,
-        });
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40],
+    zoom: 6,
+});
+
+new mapboxgl.Marker()
+    .setLngLat([-74.5, 40])
+    .addTo(map)
