21c3915f6d718d389c40eda8deb2e76f16a88850
Avdo Sator <avdo.sator@hotmail.com>
2024-02-22 21:42:09 +0100

    Change style for unclustered-point circle (single campground)

--- public/scripts/clusterMap.js
+++ public/scripts/clusterMap.js
@@ -71,8 +71,8 @@ map.on('load', () => {
         source: 'campgrounds',
         filter: ['!', ['has', 'point_count']],
         paint: {
-            'circle-color': '#11b4da',
-            'circle-radius': 4,
+            'circle-color': '#11c6da',
+            'circle-radius': 5,
             'circle-stroke-width': 1,
             'circle-stroke-color': '#fff'
         }
