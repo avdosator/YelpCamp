396d219e5fef5a8f9a99b9324231a24b2548422d
Avdo Sator <avdo.sator@hotmail.com>
2024-02-25 09:33:51 +0100

    PPosition elements on the home page

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
 app.use("/", userRoutes);
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
     res.render("home");
 });
 
